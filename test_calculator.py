e30c2b775bf1e1b42af1374cb8be7b90bd04469c
macohen <matan.workspace@gmail.com>
2020-07-23 22:09:15 +0300

    Add multiply function

--- test_calculator.py
+++ test_calculator.py
@@ -13,4 +13,4 @@ class TestCalculator:
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
-        assert 8 == calculator.multiplication(4, 2)
+        assert 8 == calculator.multiply(4, 2)
